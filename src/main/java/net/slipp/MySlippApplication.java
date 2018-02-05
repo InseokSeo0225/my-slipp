3c65318d170f4068a661c75d03a942ef26fc38fa
INSEOK-PC\Seo <insuk_seo@hotmail.com>
2018-02-05 17:32:48 +0900

    처음 commit(수정) 해줌....

--- src/main/java/net/slipp/MySlippApplication.java
+++ src/main/java/net/slipp/MySlippApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySlippApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // main이 있었지 마져...
 		SpringApplication.run(MySlippApplication.class, args);
 	}
 }
